3b0b8b7395c8c3bdc5cfa2912fea3805c176aa0b
Chris Violette <cvioletterun@gmail.com>
2017-01-18 11:19:01 -0500

    Loading of custom functionality files.

---
+++ lib/listpages.php
@@ -0,0 +1,40 @@
+<?php
+namespace PCHC\ListPages;
+
+if( !function_exists(__NAMESPACE__ . '\\list_child_pages') ) {
+  function list_child_pages( $atts ) {
+    $a = shortcode_atts(array(
+      'orderby' => 'menu_order',
+      'order' => 'DESC'
+    ), $atts );
+
+    global $post;
+    $gantry = Gantry\Framework\Gantry::instance();
+    $theme  = $gantry['theme'];
+
+    if( is_page() && $post->post_parent ){
+      //
+    } else {
+      $context              = Timber::get_context();
+
+      // WP_Query arguments
+      $args = array (
+      	'post_parent'            => $post->ID,
+      	'post_type'              => array( 'page' ),
+      	'post_status'            => array( 'publish' ),
+      	'order'                  => $a['order'],
+      	'orderby'                => $a['orderby'],
+      );
+
+      $context['posts']      = Timber::get_posts($args);
+      //$context['pagination'] = Timber::get_pagination();
+
+      $templates = ['listpages.html.twig'];
+
+      Timber::render($templates, $context);
+    }
+
+  }
+
+  add_shortcode('pchc_childpages', __NAMESPACE__ . '\\pchc_list_child_pages');
+}
